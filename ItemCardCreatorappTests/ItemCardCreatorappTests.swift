aa66d9ef55fe59b6182cce6cf3611e6f03134eb9
Sparks01 <animated00@yahoo.com>
2025-12-09 20:50:58 -0800

    Initial Commit

---
+++ ItemCardCreatorappTests/ItemCardCreatorappTests.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCardCreatorappTests.swift
+//  ItemCardCreatorappTests
+//
+//  Created by Jose Munoz on 12/9/25.
+//
+
+import Testing
+@testable import ItemCardCreatorapp
+
+struct ItemCardCreatorappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
